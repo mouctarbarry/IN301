1ed6feebbdbd0ba98032ea74316cb301caff406d
Utilisateur <user@debian.arcahe.ovh>
2019-10-03 12:49:14 +0200

    ajj

--- td2/exo11.c
+++ td2/exo11.c
@@ -10,8 +10,18 @@ int main(int argc, char const *argv[])
 	printf("double %ld octets\n", sizeof (double));
 	printf("char[7][3] %ld octets\n", sizeof (char[7][3]));
 	printf("void* %ld octets\n", sizeof (void*));
-	//printf("int [] %ld octets\n", sizeof (int[]));
+	//printf("int [] %d octets\n", sizeof (int[])); ici ça marche pas parce
 	printf("int[10] %ld octets\n", sizeof (int[10]));
 	printf("int** %ld octets\n", sizeof (int**));
+	
+	printf ("Les contenus\n");  // tab; tab[0]; &tab[0]; *&tab; *&tab[0]
+	float tab[10];
+	printf (" contenu de tab : %p\n", tab);
+	printf ("taille de tab : %ld\n", sizeof(tab));
+	printf ("contenu de tab[0] %f\n", tab[0]);
+	printf ("contenu de &tab : %p\n", &tab[0]);
+	printf ("taille de &tab[0] : %ld\n", sizeof (&tab[0]));
+	
+	
 	return 0;
 }
